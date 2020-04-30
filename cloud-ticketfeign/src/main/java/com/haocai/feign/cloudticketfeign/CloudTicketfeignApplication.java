9364b162617c6b69d16d76ae66d75fb83665b68f
CQ <caiqing@kedacom.com>
2020-04-30 16:57:03 +0800

    init2

---
+++ cloud-ticketfeign/src/main/java/com/haocai/feign/cloudticketfeign/CloudTicketfeignApplication.java
@@ -0,0 +1,13 @@
+package com.haocai.feign.cloudticketfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTicketfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudTicketfeignApplication.class, args);
+    }
+
+}
