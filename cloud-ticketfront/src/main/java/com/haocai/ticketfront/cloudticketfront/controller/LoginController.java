188e1d0b898a1fe456eb7c39a6e29e112b4eda2a
CQ <caiqing@kedacom.com>
2020-04-30 16:41:21 +0800

    init2

--- cloud-ticketfront/src/main/java/com/haocai/ticketfront/cloudticketfront/controller/LoginController.java
+++ cloud-ticketfront/src/main/java/com/haocai/ticketfront/cloudticketfront/controller/LoginController.java
@@ -1,11 +1,7 @@
 package com.haocai.ticketfront.cloudticketfront.controller;
 
-import com.haocai.feign.cloudticketfeign.service.TicketCategoryFeignService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Description TODO
@@ -24,17 +20,6 @@ public class LoginController {
     @RequestMapping("/loginJsp")
     public String loginJsp() {
         return "loginJsp";
-    }
-
-    @Autowired
-    private TicketCategoryFeignService ticketCategoryFeignService;
-
-    @GetMapping(value = "/hi")
-    @ResponseBody
-    public String sayHi() {
-        //return "a";//ticketCategoryFeignService;
-
-        return ticketCategoryFeignService.list();
     }
 
 }
