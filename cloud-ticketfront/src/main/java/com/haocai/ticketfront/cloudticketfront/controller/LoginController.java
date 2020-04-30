946f51df392390633806bc86d1294e4e89535daf
CQ <caiqing@kedacom.com>
2020-04-30 16:06:05 +0800

    init

--- cloud-ticketfront/src/main/java/com/haocai/ticketfront/cloudticketfront/controller/LoginController.java
+++ cloud-ticketfront/src/main/java/com/haocai/ticketfront/cloudticketfront/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.haocai.ticketfront.cloudticketfront.controller;
 
+import com.haocai.ticketfront.cloudticketfront.service.TicketCategoryFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Description TODO
@@ -20,6 +25,15 @@ public class LoginController {
     @RequestMapping("/loginJsp")
     public String loginJsp() {
         return "loginJsp";
+    }
+
+    @Autowired
+    TicketCategoryFeignService ticketCategoryFeignService;
+
+    @GetMapping(value = "/hi")
+    @ResponseBody
+    public String sayHi() {
+        return ticketCategoryFeignService.list();
     }
 
 }
--- cloud-ticketserver/src/main/java/com/haocai/ticketserver/cloudticketserver/CloudTicketserverApplication.java
+++ cloud-ticketserver/src/main/java/com/haocai/ticketserver/cloudticketserver/CloudTicketserverApplication.java
@@ -2,8 +2,10 @@ package com.haocai.ticketserver.cloudticketserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudTicketserverApplication {
 
     public static void main(String[] args) {
