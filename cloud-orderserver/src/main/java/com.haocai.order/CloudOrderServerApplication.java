dcd47ffae42d0558a1f62e08c49fafb3907f9d73
CQ <caiqing@kedacom.com>
2020-06-17 17:29:40 +0800

    hmily 分布式事务案例

--- cloud-orderserver/src/main/java/com.haocai.order/CloudOrderServerApplication.java
+++ cloud-orderserver/src/main/java/com.haocai.order/CloudOrderServerApplication.java
@@ -2,22 +2,31 @@ package com.haocai.order;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableEurekaClient
 @MapperScan(basePackages =
         {
                 "com.haocai.base.cloudbase.mapper"
-
         })
+@ImportResource({"classpath:applicationContext.xml"})
 @EnableScheduling
-//@EnableFeignClients
+@EnableFeignClients
+@EnableTransactionManagement
 public class CloudOrderServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
--- cloud-ticketserver/src/main/java/com/haocai/ticketserver/cloudticketserver/CloudTicketserverApplication.java
+++ cloud-ticketserver/src/main/java/com/haocai/ticketserver/cloudticketserver/CloudTicketserverApplication.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,6 +15,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
         "com.haocai.ticketserver.cloudticketserver.mapper",
         "com.haocai.base.cloudbase.mapper"
 })
+@ImportResource({"classpath:applicationContext.xml"})
+@EnableScheduling
+@EnableTransactionManagement
 public class CloudTicketserverApplication {
 
     public static void main(String[] args) {
